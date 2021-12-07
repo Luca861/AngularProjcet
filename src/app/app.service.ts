f904e13085345d692f0d47d6fbe81f72d7fdd4db
Luca861 <luca.carapellese@gmail.com>
2021-12-07 12:34:15 +0100

    feat add ex11v1

---
+++ src/app/app.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { map, Observable,of} from 'rxjs';
+import { BEERS } from './mock-data/mock-data';
+import { IBeer } from './models/beer';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  private beers:IBeer[] = BEERS;
+  private beerObservable : Observable<IBeer[]> = of(this.beers);
+
+  constructor() {
+    this.getBeers().subscribe(data =>
+      this.beers = data)
+  }
+
+    getBeers(){
+     return this.beerObservable;
+    }
+
+
+  getBeerById(id:number):Observable<IBeer>{
+  return this.beerObservable.pipe(
+     map(beers => beers.find(beer => beer.id === id))
+     )as Observable<IBeer>
+  }
+
+}
---
+++ src/app/models/beer.ts
@@ -0,0 +1,9 @@
+export type Type = "small" | "medium";
+
+export interface IBeer{
+  id:number;
+  type: Type;
+  name:string;
+  price:number;
+
+}
